3c5a4dc4ef893b7df106f37d47ae2ecc587e8854
ladlod <tian_zw@qq.com>
2020-10-13 19:36:12 +0800

    fix

--- utils.go
+++ utils.go
@@ -7,12 +7,12 @@ import (
 
 func LogError(format string, a ...interface{}) {
 	format = fmt.Sprintf("[EROR][%v] %s\n", time.Now().Format("2006-01-02 15:04"), format)
-	fmt.Printf(format, a)
+	fmt.Printf(format, a...)
 	return
 }
 
 func LogInfo(format string, a ...interface{}) {
 	format = fmt.Sprintf("[Info][%v] %s\n", time.Now().Format("2006-01-02 15:04"), format)
-	fmt.Printf(format, a)
+	fmt.Printf(format, a...)
 	return
 }
